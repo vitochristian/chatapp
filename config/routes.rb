553fa241183781b7cb349c09f55e724fafe83dec
Rails Udemy Course <rails@gmail.com>
2019-07-29 10:52:15 +0700

    Create home and login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  root "chatroom#index"
+  get 'login', to: "sessions#new"
+  get 'sessions/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
