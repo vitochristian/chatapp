43dae01705f77a05c321a8ebf8955be0236b06e3
Rails Udemy Course <rails@gmail.com>
2019-07-30 13:46:22 +0700

    Display message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,8 @@ before_action :require_user
 	def create
 		message = current_user.messages.build(message_params)
 		if message.save
-			redirect_to root_path
+			ActionCable.server.broadcast "chatroom_channel",
+										mod_message: message_render(message)
 		end
 	end
 
@@ -11,5 +12,10 @@ before_action :require_user
 	def message_params
 		params.require(:message).permit(:body)
 	end
+
+	def message_render(message)
+		render(partial: 'message', locals: {message: message})
+	end
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   post 'login', to: "sessions#create"
   delete 'logout', to: "sessions#destroy"
   post 'message', to: "messages#create"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  mount ActionCable.server, at: '/cable'
 end
