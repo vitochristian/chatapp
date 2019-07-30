04ce4869ee5d94af133ef0eeea678d873c25ff64
Rails Udemy Course <rails@gmail.com>
2019-07-30 10:18:17 +0700

    Create message with UI

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root "chatroom#index"
   get 'login', to: "sessions#new"
   get 'sessions/new'
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+  post 'message', to: "messages#create"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
